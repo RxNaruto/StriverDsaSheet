389607cb3edd819db06a2143ca81b11add881063
Rithk <rithkchaudhary86@gmail.com>
2024-06-22 21:25:41 +0530

    kadane

---
+++ Arrays/Medium/kadane.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int kadane(int arr[],int size){
+    int sum=0,max=INT_MIN,start;
+    int arrStart=-1,arrEnd=-1;
+    for(int i=0;i<size;i++){
+        if(sum==0){
+            start = i;
+        }
+       sum+=arr[i];
+       if(sum>max){
+        max=sum;
+        arrStart = start;
+        arrEnd = i;
+
+       }
+       if(sum<0){
+        sum=0;
+       }
+       
+        
+    }
+    cout<<"The index of maxsubarray is"<<endl;
+    cout<<arrStart<<" "<<arrEnd<<" "<<endl;
+    cout<<"The max sum of subarray is "<<endl;
+    return max;
+}
+int main()
+{
+     int arr[9]={-2,1,-3,4,-1,2,1,-5,4};
+     cout<<kadane(arr,9)<<endl;
+ return 0;
+}
---
+++ Arrays/Medium/maxSubarrayBetter.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int max(int a,int b){
+    if(a>b){
+        return a;
+    }
+    return b;
+}
+int maxSubarray(int arr[],int size){
+    int sum=0,maxsum=INT_MIN;
+    for(int i=0;i<size;i++){
+            sum=0;
+        for(int j=i;j<size;j++){
+        
+            sum+=arr[j];
+               maxsum = max(sum,maxsum);
+            
+        }
+    }
+    return maxsum;
+    
+}
+int main()
+{
+     int arr[9]={-2,1,-3,4,-1,2,1,-5,4};
+     cout<<maxSubarray(arr,9);
+ return 0;
+}
